ab8765449d2e5d5591c7de48e083a14c28375ac3
Tolkachev Kirill <lavcraft@gmail.com>
2017-11-05 12:20:37 +0200

    Remove unused import

--- src/main/java/com/ironbank/moneyraven/MoneyRavenApplication.java
+++ src/main/java/com/ironbank/moneyraven/MoneyRavenApplication.java
@@ -4,7 +4,6 @@ import com.ironbank.moneyraven.model.ProphetProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ProphetProperties.class)
